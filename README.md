98eccf63fd499e9de4dbcae4a78e9281b9f9d9ce
AdmiJW <jamweg22@gmail.com>
2021-07-25 23:50:31 +0800

    ReadMe Update and HelmetJS

---
+++ README.md
@@ -0,0 +1,22 @@
+# 🔗 URL Shortener Microservice Project 🔗
+
+Freecodecamp's URL Shortener Microservice Project made by AdmiJW
+
+---
+<br>
+
+## Usage
+
+Clone the repository. Run `npm i` and create respective [.env](#Environment-Variables) file. Run `npm start`.
+
+---
+<br>
+
+## Environment Variables
+
+| Environment Variable | Description |
+|-|-|
+| `NODE_ENV` | `production` or `development` |
+| `PORT` | Port to run express application on. Default is `3000` |
+| `DATABASE_URL` | Database connection URL. This application uses `Postgresql` |
+| `DATABASE_ROW_LIMIT` | Maximum number of links that can be shortened and stored in database |
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 
-//  Obtain Environment variables, only if dotenv module is installed.
-try {
-    require('dotenv').config();
-} catch (err) {}
+require('dotenv').config();
 
 
 const path = require('path');
 const express = require('express');
 const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
 const app = express();
 
 //  Utilities
@@ -17,7 +15,9 @@ const Logger = require('./utils/logger.js');
 const shortURL_new_route = require( './routes/shorturl_new.js' );
 const shortURL_visit_route = require( './routes/shorturl_visit.js' );
 
+//============================
 //  Rate Limiters
+//============================
 const apiRateLimiter = rateLimiter({
     windowMs: 1000 * 60 * 5,            // 5 Minutes, 300 seconds
     max: 600                            // Maximum of 900 requests in 5 minute
@@ -28,9 +28,18 @@ const staticRateLimiter = rateLimiter({
 }); 
 
 
-//  CORS
-//  Resolve CORS issue if the request is from some place
-//  If the environment file does not have DISABLE XORIGIN specified
+// HelmetJS for HTTP header security
+app.use(helmet({
+    contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+            scriptSrc: ["'self'", 'unpkg.com'],
+        }
+    }
+}));
+
+
+//  FreeCodeCamp's CORS Configuration
 if (!process.env.DISABLE_XORIGIN) {
     //  Apply the origin allowing middleware
     app.use(function(req, res, next) {    
@@ -45,15 +54,15 @@ if (!process.env.DISABLE_XORIGIN) {
 }
 
 
+//=======================
+// ROUTES
+//=======================
 app.use('/api/shorturl/new', apiRateLimiter, shortURL_new_route);
 app.use('/api/shorturl', apiRateLimiter, shortURL_visit_route);
 app.use('/v', apiRateLimiter, shortURL_visit_route);
 app.use('/public', staticRateLimiter, express.static( path.join(__dirname, 'public') ) );
 
 
-
-
-//  Front end page
 app.get('/', apiRateLimiter, async (req,res)=> {
     res.sendFile( path.join(__dirname, 'views', 'index.html') );
     Logger.logOriginConnected(req.headers.origin, req.ip, "HOMEPAGE");
@@ -62,4 +71,5 @@ app.get('/', apiRateLimiter, async (req,res)=> {
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log("Url Shortener Microservice started at port " + (process.env.PORT || 3000) );
+    console.log("Mode: " + process.env.NODE_ENV);
 });
